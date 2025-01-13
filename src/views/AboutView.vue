86909e01be1d0617606626be78d1627dd93c41a5
lukakurdadze <lukakurdadze2@gmail.com>
2025-01-13 18:14:43 +0400

    Initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>About This Project</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Component logic here
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="home">
+    <h1>Welcome to Web Control for Access Points</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Component logic here
+</script>
